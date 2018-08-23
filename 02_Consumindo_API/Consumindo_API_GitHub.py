12a9f490ea49e216e4a9cfdb3aaf8ccbc6c955eb
Guilherme Domith <guilhermedomith@gmail.com>
2018-08-23 20:51:04 -0300

    Adicionando cache para requisiçao

--- 02_Consumindo_API/Consumindo_API_GitHub.py
+++ 02_Consumindo_API/Consumindo_API_GitHub.py
@@ -4,16 +4,15 @@ import urllib2, json
 def main():
 
     while True:
-        login = raw_input('Login da consulta: ')
+        login = raw_input('\nLogin da consulta: ')
 
         userJson = loadJson(getUrl(login))
         if userJson == None:
             continue
 
-        continue
         print('Usuário da consulta: {}'.format(userJson['name']))
         followersJson = loadJson(getUrl(login+'/followers'))
-        print('%d seguidores:'%len(followersJson))
+        print('%d seguidores:\n'%len(followersJson))
 
         for seguidor in followersJson:
             login = seguidor['login']
@@ -30,14 +29,28 @@ def getUrl(user):
 
 def loadJson(url):
     try:
-        urlopen = urllib2.urlopen(url)
+        conteudo = ''
+        cacheFileName = url.replace('/',',') + '.cache'
+
+        ''' Abre o arquivo de cache, caso não exista cria um novo
+        e escreve o conteúdo da requisição.
+        '''
+        try:
+            cache = open(cacheFileName, 'r')
+            conteudo = cache.read()
+        except:
+            urlopen = urllib2.urlopen(url)
+            conteudo = urlopen.read()
+
+            cache = open(cacheFileName, 'w')
+            cache.write(conteudo)
+        
+        cache.close()
     except Exception as e:
         print(e)
         return None
 
-    print(urlopen)
-    print(json.load(urlopen))
-    return json.load(urlopen)
+    return json.loads(conteudo)
 
 if __name__ == "__main__":
     main()
